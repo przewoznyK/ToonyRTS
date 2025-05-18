2cd2cec8b30165ac54fb6276055552510cba5e32
przewoznyK <zs1przewozny@gmail.com>
2025-05-18 21:24:42 +0200

    Hover on Production Icon

---
+++ Assets/CancelProductionButton.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CancelProductionButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Image hoverImage;
+    private void Start()
+    {
+        SetAlpha(0f);
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        SetAlpha(1f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        SetAlpha(0f);
+    }
+
+    private void SetAlpha(float alpha)
+    {
+        Color color = hoverImage.color;
+        color.a = alpha;
+        hoverImage.color = color;
+    }
+}
